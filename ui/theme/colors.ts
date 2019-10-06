563718fdd1d0691d74e3862f875b1f8032be079a
bukinoshita <bukinoshita@gmail.com>
2019-10-06 19:38:56 -0300

    feat: add ui theme

---
+++ ui/theme/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  white: '#ffffff',
+  black: '#000000',
+  primary: {
+    default: '#3B3DBF',
+    soft: '#edf2ff',
+    lighter: '#d9e3ff',
+    dark: '#003ad4'
+  },
+  gray: {
+    default: '#777677',
+    darker: '#222222',
+    lighter: '#dddfe4'
+  },
+  title: '#444444',
+  text: '#869ab8'
+}
