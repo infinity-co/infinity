7e524c496bf3f701ee05139e3ce8190177757336
bukinoshita <bukinoshita@gmail.com>
2019-10-06 19:39:30 -0300

    feat: add ui row

---
+++ ui/row/row.tsx
@@ -0,0 +1,30 @@
+import React, { PureComponent } from 'react'
+
+// UI
+import { spacing } from '../theme'
+
+// Interface
+import { RowProps } from './row.interface'
+
+export class Row extends PureComponent<RowProps> {
+  render() {
+    const { children, style } = this.props
+
+    return (
+      <div style={style}>
+        {children}
+
+        <style jsx={true}>{`
+          div {
+            width: 100%;
+            max-width: 1000px;
+            margin-left: auto;
+            margin-right: auto;
+            padding-left: ${spacing.xLarge};
+            padding-right: ${spacing.xLarge};
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
