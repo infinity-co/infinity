aaa290ea923fb7ba6f69ecbe5068544e9fff8bd8
bukinoshita <bukinoshita@gmail.com>
2019-10-10 15:52:49 -0300

    chore: add authentication service env var

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 module.exports = {
   target: 'serverless',
   env: {
-    STATICKIT_ID: process.env.STATICKIT_ID
+    STATICKIT_ID: process.env.STATICKIT_ID,
+    AUTHENTICATION_SERVICE: process.env.AUTHENTICATION_SERVICE
   }
 }
